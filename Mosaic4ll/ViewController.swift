a57cb3acfc6a7b84bf422f72a66049d973dcfb13
Michael <liukaiyi54@gmail.com>
2017-03-31 09:23:01 +0800

    Initial Commit

---
+++ Mosaic4ll/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Mosaic4ll
+//
+//  Created by Michael on 31/03/2017.
+//  Copyright © 2017 Michael. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
